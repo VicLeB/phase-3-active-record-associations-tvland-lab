c559c2209f5fac2e506a0ebfc0e83562979dce60
Victoria LeBel <vickileb@hotmail.com>
2022-07-27 08:54:59 -0700

    commit tree 2e5b011b7f29cbd5011f94dcad74f0af4fb36cd4

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+
+    belongs_to :network
+
+    def actors_list
+        cast = self.characters
+        cast.map{|c| "#{c.actor.first_name} #{c.actor.last_name}" }
+        #characters.map{|c| "#{c.actor.full_name}"}
+    end
 end
